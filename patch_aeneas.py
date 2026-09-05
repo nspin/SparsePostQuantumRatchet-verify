be86382bba80b9f5383df83445273284fce37f31
Nick Spinale <nick@nickspinale.com>
2026-09-05 02:31:03 -0700

    x

--- patch_aeneas.py
+++ patch_aeneas.py
@@ -1,1 +1,25 @@
 #!/usr/bin/env python3
+
+from pathlib import Path
+
+FILES = [
+    'Types.lean',
+    'Funs.lean',
+]
+
+ROOT = Path('foo/SrcTranslated')
+
+def run():
+    for f in FILES:
+      p = ROOT / f
+      encoding = 'utf-8'
+      s = p.read_text(encoding=encoding)
+      patch(s)
+      p.write_text(s, encoding=encoding)
+
+def patch(s):
+    pass
+
+
+if __name__ == '__main__':
+    run()
