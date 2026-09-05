92b424c08f5263aeda6cea3f4755e0eca8a83db9
Nick Spinale <nick@nickspinale.com>
2026-09-05 12:28:11 -0700

    x

--- flake.nix
+++ flake.nix
@@ -6,22 +6,15 @@
     aeneas.url = "github:AeneasVerif/aeneas/nightly-2026.08.27-5b9dcf3";
   };
 
-  outputs = inputs @ { self, nixpkgs, flake-utils, charon, aeneas, ... }:
+  outputs = inputs@{ self, nixpkgs, flake-utils, charon, aeneas, ... }:
     flake-utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
         charon = inputs.charon.packages.${system}.charon;
         aeneas = inputs.aeneas.packages.${system}.aeneas;
-      in
-      {
+      in {
         devShells.default = pkgs.mkShell {
-          packages = [
-            pkgs.python3
-            pkgs.protobuf
-            pkgs.elan
-            charon
-            aeneas
-          ];
+          packages = [ pkgs.python3 pkgs.protobuf pkgs.elan charon aeneas ];
         };
       });
 }
