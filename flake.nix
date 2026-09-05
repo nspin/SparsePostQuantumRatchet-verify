0ca4cf3e1fb57a899d116dba48e201c2c7e7ad0a
Nick Spinale <nick@nickspinale.com>
2026-09-05 01:33:33 -0700

    x

---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "Aeneas";
+
+  inputs = {
+    nixpkgs.follows = "aeneas/nixpkgs";
+    flake-utils.follows = "aeneas/flake-utils";
+    charon.follows = "aeneas/charon";
+    aeneas.url = "github:AeneasVerif/aeneas";
+  };
+
+  outputs = inputs @ { self, nixpkgs, flake-utils, charon, aeneas, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = import nixpkgs { inherit system; };
+        charon = inputs.charon.packages.${system}.charon;
+        aeneas = inputs.aeneas.packages.${system}.aeneas;
+      in
+      {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.protobuf
+            pkgs.elan
+            charon
+            aeneas
+          ];
+        };
+      });
+}
